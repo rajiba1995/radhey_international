3488c974323359c783fee6f08e68fb2e23eee74c
Junior_Developer_Souvik <souvikmandal.sm19@gmail.com>
2024-12-04 11:57:45 +0530

    Update 1-12-2024

---
+++ app/Http/Livewire/MasterSubCategory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class MasterSubCategory extends Component
+{
+    
+    public function render()
+    {
+        return view('livewire.master-sub-category');
+    }
+}
