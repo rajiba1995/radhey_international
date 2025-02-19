bc9cd3093ff0bdfadd9538657ab1e6a0744cfc61
papiyahalder <papiyahalder737@gmail.com>
2025-02-19 20:30:58 +0530

    ledger

--- app/Models/Ledger.php
+++ app/Models/Ledger.php
@@ -8,29 +8,41 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Ledger extends Model
 {
     use HasFactory;
+
     protected $table = 'ledgers';
+
     protected $fillable = [
-        'user_type', 'staff_id', 'customer_id', 'supplier_id', 'admin_id', 'payment_id', 'staff_commision_id', 'collection_staff_commission_id', 'store_bad_debt_id', 'transaction_id', 'transaction_amount', 'is_credit', 'is_debit', 'bank_cash', 'entry_date', 'purpose', 'purpose_description', 'start_date', 'whatsapp_status', 'last_whatsapp', 'created_at', 'updated_at'
+        'user_type', 'staff_id', 'customer_id', 'supplier_id', 'admin_id', 
+        'payment_id', 'staff_commision_id', 'collection_staff_commission_id', 
+        'store_bad_debt_id', 'transaction_id', 'transaction_amount', 
+        'is_credit', 'is_debit', 'bank_cash', 'entry_date', 'purpose', 
+        'purpose_description', 'start_date', 'whatsapp_status', 
+        'last_whatsapp', 'created_at', 'updated_at'
     ];
 
     public function staff()
     {
-        return $this->belongsTo(User::class, 'staff_id', 'id');
+        return $this->belongsTo(User::class, 'staff_id');
     }
+
     public function customer()
     {
-        return $this->belongsTo(User::class, 'customer_id', 'id');
+        return $this->belongsTo(User::class, 'customer_id');
     }
+
     public function supplier()
     {
-        return $this->belongsTo(Supplier::class, 'supplier_id', 'id');
+        return $this->belongsTo(Supplier::class, 'supplier_id');
     }
+
     public function admin()
     {
-        return $this->belongsTo(User::class, 'admin_id', 'id');
+        return $this->belongsTo(User::class, 'admin_id');
     }
+
     public function payment()
     {
-        return $this->belongsTo(Payment::class, 'payment_id', 'id');
+        return $this->belongsTo(Payment::class, 'payment_id');
     }
 }
+
